5fbaa8e5bd102c5971d5ebfe40534dbfc982987b
NileshS236 <nileshsamota0602@gmail.com>
2020-11-05 00:24:51 +0530

    commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCWLp8Xf-84nBNyD3_V4DXCV95b0zOyxM0",
+  authDomain: "nilesh-samota-portfolio.firebaseapp.com",
+  databaseURL: "https://nilesh-samota-portfolio.firebaseio.com",
+  projectId: "nilesh-samota-portfolio",
+  storageBucket: "nilesh-samota-portfolio.appspot.com",
+  messagingSenderId: "643781500372",
+  appId: "1:643781500372:web:bc915e1f9b30f37f35ca41",
+  measurementId: "G-M3BR64YBTT",
+};
